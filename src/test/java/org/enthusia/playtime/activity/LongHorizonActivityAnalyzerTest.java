acd2cb00f25ff71eba4c9fb73cc28e16890d7339
P2wn <racecarboy77@gmail.com>
2026-09-02 16:59:48 -0400

    Add sparse activity regression coverage

--- src/test/java/org/enthusia/playtime/activity/LongHorizonActivityAnalyzerTest.java
+++ src/test/java/org/enthusia/playtime/activity/LongHorizonActivityAnalyzerTest.java
@@ -27,16 +27,31 @@ class LongHorizonActivityAnalyzerTest {
     }
 
     @Test
-    void oneInputEveryTwoSecondsIsStillTooSparseToTrust() {
+    void ordinaryTwoSecondInputCadenceIsNotSparseKeepaliveEvidence() {
         Deque<Long> pulses = new ArrayDeque<>();
-        for (long time = 1_000L; time <= 91_000L; time += 2_000L) {
+        for (long time = 1_000L; time <= 121_000L; time += 2_000L) {
             LongHorizonActivityAnalyzer.recordPulse(pulses, time);
         }
 
-        double evidence = LongHorizonActivityAnalyzer.sparseKeepaliveEvidence(
-                pulses, 91_500L, IDLE_MILLIS);
+        assertEquals(0.0D, LongHorizonActivityAnalyzer.sparseKeepaliveEvidence(
+                pulses, 121_500L, IDLE_MILLIS));
+    }
 
-        assertTrue(evidence >= 0.99D);
+    @Test
+    void longHorizonVerySparseHumanizedKeepaliveStillBecomesSuspicious() {
+        Deque<Long> pulses = new ArrayDeque<>();
+        long time = 1_000L;
+        int[] gaps = {11_000, 14_000, 9_000, 16_000, 12_000, 13_000};
+        int index = 0;
+        while (time <= 241_000L) {
+            LongHorizonActivityAnalyzer.recordPulse(pulses, time);
+            time += gaps[index++ % gaps.length];
+        }
+
+        double evidence = LongHorizonActivityAnalyzer.sparseKeepaliveEvidence(
+                pulses, 241_500L, IDLE_MILLIS);
+
+        assertTrue(evidence >= 0.90D);
     }
 
     @Test
